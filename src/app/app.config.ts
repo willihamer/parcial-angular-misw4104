444cd22fe02bd5e4477e5d484dc1897f34dbfd9a
Hamer <hamer.patarroyo@gmail.com>
2026-04-25 11:47:09 -0500

    create navbar and implement sytles from stitch

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+  ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideNavComponent } from './components/side-nav/side-nav';
+import { TopBarComponent } from './components/top-bar/top-bar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, SideNavComponent, TopBarComponent, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('parcial-angular-misw4104');
+  isSideNavOpen = false;
+
+  toggleSideNav(): void {
+    this.isSideNavOpen = !this.isSideNavOpen;
+  }
+
+  closeSideNav(): void {
+    this.isSideNavOpen = false;
+  }
 }
